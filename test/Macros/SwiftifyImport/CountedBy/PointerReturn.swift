2ce21f552189b8652cf7390b11c965be86c755d0
Henrik G. Olsson <h_olsson@apple.com>
2024-12-18 17:56:17 +0100

    [Swiftify] Import __counted_by for function return values
    Instead of simply passing a parameter index to _SwiftifyInfo, the
    _SwiftifyExpr enum is introduced. It currently has two cases:
     - .param(index: Int), corresponding to the previous parameter index
     - .return, corresponding to the function's return value.
    
    ClangImporter is also updated to pass this new information along to
    _SwiftifyImport, allowing overloads with buffer pointer return types to
    be generated. The swiftified return values may not yet map to Span
    types, because we don't currently track the lifetime information
    necessary to do so.

--- test/Macros/SwiftifyImport/CountedBy/PointerReturn.swift
+++ test/Macros/SwiftifyImport/CountedBy/PointerReturn.swift
@@ -2,11 +2,12 @@
 
 // RUN: %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -dump-macro-expansions 2>&1 | %FileCheck --match-full-lines %s
 
-@_SwiftifyImport(.countedBy(pointer: .param(1), count: "len"))
-func myFunc(_ ptr: UnsafePointer<CInt>, _ len: CInt) -> CInt {
+@_SwiftifyImport(.countedBy(pointer: .return, count: "len"))
+func myFunc(_ len: CInt) -> UnsafeMutablePointer<CInt> {
 }
 
 // CHECK:      @_alwaysEmitIntoClient
-// CHECK-NEXT: func myFunc(_ ptr: UnsafeBufferPointer<CInt>) -> CInt {
-// CHECK-NEXT:     return myFunc(ptr.baseAddress!, CInt(exactly: ptr.count)!)
+// CHECK-NEXT: func myFunc(_ len: CInt) -> UnsafeMutableBufferPointer<CInt> {
+// CHECK-NEXT:     return UnsafeMutableBufferPointer<CInt> (start: myFunc(len), count: Int(len))
 // CHECK-NEXT: }
+
